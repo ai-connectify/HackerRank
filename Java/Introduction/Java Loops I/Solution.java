aa15ae118b56786a03141a0506a748f4be06f6c4
ai-connectify <95274928+ai-connectify@users.noreply.github.com>
2021-12-05 16:54:09 +0530

    Create Solution.java

---
+++ Java/Introduction/Java Loops I/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int i;
+        for(i=1 ; i<=10 ; i++){
+            System.out.println(N+" x "+i+" = "+N*i);
+        }
+    }
+}
