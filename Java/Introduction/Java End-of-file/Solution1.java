3ededfc22c8d7a06723b43c29ed62346af7932dd
ai-connectify <95274928+ai-connectify@users.noreply.github.com>
2021-12-11 11:37:11 +0530

    Create Solution1.java
    Solution.java : Using While loop
    Solution1.java : Using For loop

---
+++ Java/Introduction/Java End-of-file/Solution1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+       for(int i = 1; sc.hasNext() ;i++) {
+              String s = sc.nextLine();
+              System.out.println(i+" "+s);    
+        }
+        
+    }
+}
+
