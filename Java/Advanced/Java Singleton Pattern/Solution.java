bdc8b181eef32c05195f6f1c07180ebb1625be59
ai-connectify <95274928+ai-connectify@users.noreply.github.com>
2021-12-28 15:05:06 +0530

    Create Solution.java

---
+++ Java/Advanced/Java Singleton Pattern/Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+class Singleton{
+    public String str;
+    private static Singleton s;
+    private Singleton(){
+  
+    }
+   public static Singleton getSingleInstance(){
+        return new Singleton();
+    }
+}
